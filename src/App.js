5a6ec2684ea8982efe871f679a6e6bad5dca6c46
Zain <imxayn2000@gmail.com>
2020-02-14 13:32:19 -0800

    updats

--- src/App.js
+++ src/App.js
@@ -2,29 +2,25 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 
-
-
 import { Grid } from '@material-ui/core';
 
-// const useStyles = makeStyles(theme => ({
-//   main:{
-//     //padding:200
-//   }
-// }));
 
 function App() {
-  //const classes = useStyles();
   const [button, setButton] = useState([])
   const [bar, setBar] = useState([])
   const [limit, setLimit] = useState()
-  //const [value, setValue] = useState()
 
-  const progressBar = [1, 2, 3]
+
 
   const handleButton = (event) => {
     const value = parseInt(event.target.value)
-    setBar(bar.map(num => num + value))
+    setBar(bar.map(num => (
+      num + value > 0 ? (num + value) : 0
+    )))
+  }
 
+  const handleProgress = () => {
+    console.log('progress')
   }
 
 
@@ -32,14 +28,12 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get('http://pb-api.herokuapp.com/bars')
-      console.log(response)
       setButton(response.data.buttons)
-      setBar(response.data.bars.slice(0, 3))
+      setBar(response.data.bars)
       setLimit(response.data.limit)
     }
     fetchData()
   }, []);
-  console.log(bar)
 
 
   return (
@@ -55,23 +49,28 @@ function App() {
         <br />
         {bar && bar.map((num, index) => (
           <>
-            <div className="w3-border">
-              <div className="w3-blue" style={{ height: 24, width: `${num}%`, background: 'red', maxWidth: `${limit}%` }}>
-                <span>{num}%</span>
+            <div className="w3-border" style={{ maxWidth: `${limit}%` }}>
+
+              <div className="w3-blue" style={{ height: 24, width: `${num > 0 ? num : 0}%`, background: 'red !important', maxWidth: `${limit}%`, minWidth: 0 }}>
+                <span>{num > 0 ? `${num}%` : 0}</span>
               </div>
+
             </div>
           </>
         ))}
         <br />
-
-
         <select>
-          <option value="progress1">progress 1</option>
-          <option value="progress2">progress 2</option>
-          <option value="progress3"> progress 3</option>
+          {bar && bar.map((num, index) => (
+
+            <option value="progress1" >progress {index + 1}</option>
+
+          ))}
         </select>
 
+
+
         {button && button.map((btn, index) => (
+
           <input type="button" name="name" value={btn} onClick={handleButton} />
         ))}
 
