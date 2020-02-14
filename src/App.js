414ab3d6d3816fda5eadd201c43cebdbc3a207c4
Zain <imxayn2000@gmail.com>
2020-02-14 11:35:34 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,93 +1,87 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 
 
-import './App.css';
-import { LinearProgress, Grid } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  main:{
-    padding:200
-  }
-}));
+import { Grid } from '@material-ui/core';
+
+// const useStyles = makeStyles(theme => ({
+//   main:{
+//     //padding:200
+//   }
+// }));
 
 function App() {
-  const classes = useStyles();
+  //const classes = useStyles();
   const [button, setButton] = useState([])
   const [bar, setBar] = useState([])
   const [limit, setLimit] = useState()
   const [value, setValue] = useState()
-  
-  const handleButton = event => {
-    console.log(event.target.value)
-    
-    
-    
-    setValue(event.target.value)
+
+  const progressBar = [1, 2, 3]
+
+  const handleButton = (event) => {
+    //setBar(event.target.value)
+    //setValue(event.target.value + value)
+    const val = event.target.value
+    console.log(val,'val!')
+    bar.map(num => num + val)
+    console.log(bar.map(num => num + val),'bars...')
   }
 
-  
 
-  useEffect( () => {
-    async function fetchData(){
-    const response = await axios.get('http://pb-api.herokuapp.com/bars')
-    setButton(response.data.buttons)
-    setBar(response.data.bars)
-    setLimit(response.data.limit)
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get('http://pb-api.herokuapp.com/bars')
+      console.log(response)
+      setButton(response.data.buttons)
+      setBar(response.data.bars.slice(0,3))
+      setLimit(response.data.limit)
     }
     fetchData()
-   }, []);
+  }, []);
+  console.log(bar)
+
 
   return (
     <Grid
-    container
-    direction="row"
-    justify="center"
-    alignItems="center"
-    className={classes.main}
-  >
-   <div style={{textAlign: 'center'}}>
-      <h4>Progress Bar</h4>
-      <br />
-   
-  <div className="progress">
-    <div className="progress-bar" role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={{width: `${value}%`}}>
-    {value > 0 && (
-      <span>{value}%</span>
-    )}  
-    </div>
-  </div>
-  <div className="progress">
-    <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{width: `${value}%`}}>
-    {value > 0 && (
-      <span>{value}%</span>
-    )}  
-    </div>
-  </div>
-  <div className="progress">
-    <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style={{width: `${value}%`}}>
-    {value > 0 && (
-      <span>{value}%</span>
-    )}  
-   
-    </div>
-  </div>
-  <select>
-  <option value="progress1">progress 1</option>
-  <option value="progress2">progress 2</option>
-  <option value="progress3"> progress 3</option>
-</select>
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    //className={classes.main}
+    >
+      <div style={{ textAlign: 'center' }}>
+        <h4>Progress Bar</h4>
+        <br />
+        {bar && bar.map((num,index) => (
+          <>
+          <div className="w3-border">
+          <div className="w3-blue" style={{ height: 24, width: `${num}%`, background: 'red' }}>
+            <span>{num}%</span>
+          </div>
+          </div>
+          </>
+        ))}
+        <br />
 
 
-    {button&& button.map(btn => (
-      <input type="button" name="name" value={btn} onClick={handleButton}/>
-    ))}
+        <select>
+          <option value="progress1">progress 1</option>
+          <option value="progress2">progress 2</option>
+          <option value="progress3"> progress 3</option>
+        </select>
 
-    {console.log(limit,'limit!')}
-   
-   </div>
-   </Grid>
+        {button && button.map((btn,index) => (
+          <input type="button" name="name" value={btn} onClick={ handleButton} />
+        ))}
+
+        
+
+      </div>
+    </Grid>
   );
 }
 
